beb40347a6f19a31bb76dd149ec8f963298bed3f
cmdtjeff <cmdtjeff@gmail.com>
2023-09-12 11:21:40 -0300

    refatorando com strategy

--- src/main/java/br/com/example/model/Desempenho.java
+++ src/main/java/br/com/example/model/Desempenho.java
@@ -1,5 +1,26 @@
 package br.com.example.model;
 
+import java.math.BigDecimal;
+
 public enum Desempenho {
-    A_DESEJAR, BOM, OTIMO;
+    A_DESEJAR {
+        @Override
+        public BigDecimal percenteualReajuste() {
+            return new BigDecimal("0.03");
+        }
+    },
+    BOM {
+        @Override
+        public BigDecimal percenteualReajuste() {
+            return new BigDecimal("0.15");
+        }
+    },
+    OTIMO {
+        @Override
+        public BigDecimal percenteualReajuste() {
+            return new BigDecimal("0.2");
+        }
+    };
+
+    public abstract BigDecimal percenteualReajuste();
 }
