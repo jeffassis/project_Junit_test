847a0029493d6eec9e2d25e02a88e8350b80ceea
cmdtjeff <cmdtjeff@gmail.com>
2023-09-12 10:09:45 -0300

    first commit

---
+++ src/main/java/br/com/example/service/BonusService.java
@@ -0,0 +1,17 @@
+package br.com.example.service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import br.com.example.model.Funcionario;
+
+public class BonusService {
+
+    public BigDecimal calcularBonus(Funcionario funcionario) {
+        BigDecimal valor = funcionario.getSalario().multiply(new BigDecimal("0.1"));
+        if (valor.compareTo(new BigDecimal("1000")) > 0) {
+            valor = BigDecimal.ZERO;
+        }
+        return valor.setScale(2, RoundingMode.HALF_UP);
+    }
+}
